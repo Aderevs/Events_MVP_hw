c44d86fd4912b8e0019f021e4531e63316232aad
Aderevs <aderevs@gmail.com>
2024-05-10 11:00:55 +0300

    Last change

--- Program.cs
+++ Program.cs
@@ -7,9 +7,34 @@ namespace Events_MVP_hw
     {
         static void Main(string[] args)
         {
-            User mainUser = new User("user1");
-            User otherUser = new User("user2");
-            new Presenter(mainUser, new UserInterface());
+            bool exit = false;
+            while (!exit)
+            {
+                Console.Clear();
+                try
+                {
+                    User mainUser = new User("user1");
+                    User otherUser = new User("user2");
+                    new Presenter(mainUser, new UserInterface());
+                }
+                catch (FormatException ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("\n\n\n");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine(ex.ToString());
+                    Console.ResetColor();
+                    Console.WriteLine("\n\n\n");
+                    Console.ReadKey();
+                }
+            }
+
         }
     }
 }
